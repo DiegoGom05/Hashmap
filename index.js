966885ef05232858d2fd413188976417c04577ca
volkycode <volkycode@gmail.com>
2024-02-18 22:33:00 -0600

    Hashmap
    Added new functions

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ class HashMap {
     }
     hash(key) {
         let hashCode = 0;
-           
         const primeNumber = 31;
         for (let i = 0; i < key.length; i++) {
           hashCode = primeNumber * hashCode + key.charCodeAt(i);
@@ -21,11 +20,9 @@ class HashMap {
     }
     get(key){
         const index = this.hash(key);
-        const bucket = this.buckets[index];
-
         if(this.buckets[index]){
             for(let i = 0; i < this.buckets[index].length; i++){
-                if(this.buckets[index][i].key == key){
+                if(this.buckets[index][i].key === key){
                     return this.buckets[index][i].value;
                 }
             }
@@ -60,7 +57,42 @@ class HashMap {
         }
         return count;
     }
-
+    clear(){
+       this.buckets.splice(0, this.buckets.length)
+    }
+    keys(){
+        let keys = [];
+        for(let i = 0; i < this.buckets.length; i++){
+            if(this.buckets[i]){
+                for(let j = 0; j< this.buckets[i].length; j++){
+                    keys.push(this.buckets[i][j].key)
+                }
+            }
+        }
+        return keys;
+    } 
+    values(){
+        let values = [];
+        for(let i = 0; i < this.buckets.length; i++){
+            if(this.buckets[i]){
+                for(let j = 0; j< this.buckets[i].length; j++){
+                    values.push(this.buckets[i][j].value)
+                }
+            }
+        }
+        return values;
+    }
+    entries(){
+        let entries = [];
+        for(let i = 0; i < this.buckets.length; i++){
+            if(this.buckets[i]){
+                for(let j = 0; j< this.buckets[i].length; j++){
+                    entries.push(this.buckets[i][j])
+                }
+            }
+        }
+        return entries;
+    }
 }
 
 hashMap = new HashMap;
@@ -68,5 +100,9 @@ hashMap.set(hashMap.hash('hello'), 'brother');
 console.log(hashMap.length())
 console.log(hashMap);
 console.log(hashMap.get(hashMap.hash('hello')));
-hashMap.hash('hello')
- 
+hashMap.set(hashMap.hash('kanye'), 'west');
+console.log(hashMap.keys());
+console.log(hashMap.values());
+console.log(hashMap.entries());
+hashMap.clear();
+console.log(hashMap);
